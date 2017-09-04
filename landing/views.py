1884e5b8430f19e6aae59653ea0abc19b5d6ffc4
admin <admin@gmail.com>
2017-09-04 20:17:26 +0300

    Second commit

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .forms import SubscriberForm
 
 def landing(request):
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        print (form.cleaned_data)
+        data = form.cleaned_data
+
+        new_form = form.save()
+
+
     return render(request, 'landing/landing.html', locals() )
