f5cf171ae53fc1d1290c003252430f226f5cd485
admin <admin@gmail.com>
2017-09-04 21:46:37 +0300

    Third commit

--- landing/admin.py
+++ landing/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Subscribers)
+class SubscriberAdmin(admin.ModelAdmin):
+    #list_display = ['name', 'email']
+    list_display =  [field.name for field in Subscribers._meta.fields]
+    #fields = ['name']
+    #exclude = ['email']
+    list_filter = ['name',]
+    search_fields = ['name']
+    class Meta:
+        model = Subscribers
+
+admin.site.register(Subscribers, SubscriberAdmin)
